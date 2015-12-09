5c91b39c85aeed9f9b7defe307c21797f1415e90
Tony R <tonydraf@gmail.com>
2015-12-09 15:43:03 +0100

    add basic class container for data, based on google mnist image loader

--- libs/orlfaces.py
+++ libs/orlfaces.py
@@ -1,6 +1,36 @@
 from scipy import misc
 import os
 
+class Data(object):
+    def __init__(self, images, labels):
+        assert images.shape[0] == labels.shape[0], (
+            "images.shape: %s labels.shape: %s" % (images.shape,
+                                                   labels.shape))
+        self._num_examples = images.shape[0]
+        # Convert shape from [num examples, rows, columns, depth]
+        # to [num examples, rows*columns] (assuming depth == 1)
+        assert images.shape[3] == 1
+        images = images.reshape(images.shape[0],
+                                images.shape[1] * images.shape[2])
+        # Convert from [0, 255] -> [0.0, 1.0].
+        images = images.astype(numpy.float32)
+        images = numpy.multiply(images, 1.0 / 255.0)
+        self._images = images
+        self._labels = labels
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def num_examples(self):
+        return self._num_examples
+
+
 if __name__ == "__main__":
     import sys
 
