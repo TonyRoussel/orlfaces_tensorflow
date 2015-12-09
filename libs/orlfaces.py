361939b3325ba58985cfda5ac80e77de3e1277d9
shortyp <tonydraf@gmail.com>
2015-12-09 20:54:26 +0100

    add Datas class for train/test differenciation and implement in orlfaces_loader

--- libs/orlfaces.py
+++ libs/orlfaces.py
@@ -2,6 +2,10 @@ from scipy import misc
 import os
 import numpy as np
 
+
+class Datas(object):
+    pass
+
 class Data(object):
     def __init__(self, images, labels):
         assert images.shape[0] == labels.shape[0], (
@@ -18,6 +22,9 @@ class Data(object):
         images = np.multiply(images, 1.0 / 255.0)
         self._images = images
         self._labels = labels
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
+        return
 
     @property
     def images(self):
@@ -30,6 +37,29 @@ class Data(object):
     @property
     def num_examples(self):
         return self._num_examples
+
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
+
+    def next_batch(self, batch_size):
+        """Return the next `batch_size` examples from this data set."""
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+        if self._index_in_epoch > self._num_examples:
+            # Finished epoch
+            self._epochs_completed += 1
+            # Shuffle the data
+            perm = numpy.arange(self._num_examples)
+            numpy.random.shuffle(perm)
+            self._images = self._images[perm]
+            self._labels = self._labels[perm]
+            # Start next epoch
+            start = 0
+            self._index_in_epoch = batch_size
+            assert batch_size <= self._num_examples
+        end = self._index_in_epoch
+        return self._images[start:end], self._labels[start:end]
 
 
 def orlfaces_loader(paths):
@@ -47,13 +77,30 @@ def orlfaces_loader(paths):
             mtx_stack.append(img)
             lbl_stack.append(label)
         class_i = class_i + 1
-    num_imgs = len(mtx_stack)
     rows = mtx_stack[0].shape[0]
     cols = mtx_stack[0].shape[1]
     channel = 1
-    imgs = np.concatenate(mtx_stack, axis=1).reshape(num_imgs, rows, cols, channel)
-    labels = np.vstack(lbl_stack)
-    return Data(imgs, labels)
+
+    train, test = split_list(zip(mtx_stack, lbl_stack), 70)
+    imgs_train_stack, labels_train_stack = zip(*train)
+    imgs_test_stack, labels_test_stack = zip(*test)
+    
+    imgs_train = np.concatenate(imgs_train_stack, axis=1).reshape(len(imgs_train_stack), rows, cols, channel)
+    imgs_test = np.concatenate(imgs_test_stack, axis=1).reshape(len(imgs_test_stack), rows, cols, channel)
+    labels_train = np.vstack(labels_train_stack)
+    labels_test = np.vstack(labels_test_stack)
+    datas = Datas()
+    datas.train = Data(imgs_train, labels_train)
+    datas.test = Data(imgs_test, labels_test)
+    return datas
+
+
+def split_list(mylist, percent):
+    lft_sze = int(percent / 100. * len(mylist))
+    rht_sze = len(mylist) - lft_sze
+    lft = mylist[:lft_sze]
+    rht = mylist[-rht_sze:]
+    return (lft, rht)
                 
 
 
@@ -62,9 +109,15 @@ if __name__ == "__main__":
 
 
     orlfaces = orlfaces_loader(sys.argv[1:])
-    print "type(orlfaces.images)"
-    print type(orlfaces.images) # expect <type 'numpy.ndarray'>
-    print orlfaces.images.shape # expect tuple w\ (num imgs, img flat size)
-    print "type(orlfaces.labels)"
-    print type(orlfaces.labels) # expect <type 'numpy.ndarray'>
-    print orlfaces.labels.shape # expect tuple w\ (num imgs, number of class aka number of path)
+    print "type(orlfaces.train.images)"
+    print type(orlfaces.train.images) # expect <type 'numpy.ndarray'>
+    print orlfaces.train.images.shape # expect tuple w\ (num imgs, img flat size)
+    print "type(orlfaces.train.labels)"
+    print type(orlfaces.train.labels) # expect <type 'numpy.ndarray'>
+    print orlfaces.train.labels.shape # expect tuple w\ (num imgs, number of class aka number of path)
+    print "type(orlfaces.test.images)"
+    print type(orlfaces.test.images) # expect <type 'numpy.ndarray'>
+    print orlfaces.test.images.shape # expect tuple w\ (num imgs, img flat size)
+    print "type(orlfaces.test.labels)"
+    print type(orlfaces.test.labels) # expect <type 'numpy.ndarray'>
+    print orlfaces.test.labels.shape # expect tuple w\ (num imgs, number of class aka number of path)
